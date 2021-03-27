6f6b7a45fe1e1090c3b1fe560a6c28d94603d503
Valer1ya <lera.grigoreva@list.ru>
2021-03-27 17:07:52 +0600

    lr5

---
+++ TRPO-lab5/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TRPO_lab5.Models;
+using TRPO_Lab3.Lib;
+
+namespace TRPO_lab5.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index(string radius, string radius1, string length)
+        {
+            double Square = Math.Round(Math_1.Formula(Convert.ToDouble(radius), Convert.ToDouble(radius1), Convert.ToDouble(length)), 2);
+            ViewBag.result = Square;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
