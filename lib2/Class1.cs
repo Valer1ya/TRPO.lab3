3463cb2a86c6d653d0e5449a51c91f0991eb792f
Valer1ya <lera.grigoreva@list.ru>
2021-03-27 18:38:16 +0600

    lr4

---
+++ lib2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib2
+{
+    public class Math_2
+    {
+        public static double Formula(double r, double r1, double l)
+        {
+            if (r <= 0)
+            {
+                throw new ArgumentException("Переменная h не может быть отрицательной");
+            }
+            if (r1 <= 0)
+            {
+                throw new ArgumentException("Переменная r1 не может быть отрицательной");
+            }
+            if (l <= 0)
+            {
+                throw new ArgumentException("Переменная l не может быть отрицательной");
+            }
+            return Math.PI * (r + r1) * l;
+        }
+    }
+}
